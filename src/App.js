e0bf5ac1beba0e0bb9ef7443f336362114d9ff50
abilnf <slin.lbeog@gmail.com>
2022-02-14 11:18:53 +0100

    Add light/dark theme

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components/macro";
 import { useToggle } from "./utility/hooks";
 
-function App() {
+// connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
+// connectFirestoreEmulator(db, "localhost", 8080);
+// connectStorageEmulator(storage, "localhost", 9199);
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 30% 33% 37%;
+
+  div {
+    height: 100vh;
+  }
+
+  div:not(:last-child) {
+    border-right: 1px solid ${(props) => props.theme.bd};
+  }
+`;
+
+function App(props) {
   const [theme, toggleTheme] = useToggle(true);
 
-  return <ThemeProvider theme={theme ? lightTheme : darkTheme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme ? lightTheme : darkTheme}>
+      <Grid></Grid>
+    </ThemeProvider>
+  );
 }
 
 const lightTheme = {
   p: "#f57c00",
-  pl: "#ffad42",
   pd: "#bb4d00",
-
-  bg: "#fafafa",
-  bgl: "#ffffff",
-  bgd: "#c7c7c7",
+  s: "#000000",
+  sd: "#212121",
+  b: "#ffffff",
+  bd: "#eeeeee",
+  f: "#000000",
+  fs: "#484848",
+  fi: "#ffffff",
 };
 
 const darkTheme = {
   p: "#f57c00",
-  pl: "#ffad42",
   pd: "#bb4d00",
-
-  bg: "#fafafa",
-  bgl: "#ffffff",
-  bgd: "#c7c7c7",
+  s: "#ffffff",
+  sd: "f5f5f5",
+  b: "#000000",
+  bd: "#212121",
+  f: "#ffffff",
+  fs: "#484848",
+  fi: "#000000",
 };
+
+// const lightTheme = {
+//   p: "#f57c00",
+//   pl: "#ffad42",
+//   pd: "#bb4d00",
+
+//   bg: "#fafafa",
+//   bgl: "#ffffff",
+//   bgd: "#c7c7c7",
+// };
+
+// const darkTheme = {
+//   p: "#f57c00",
+//   pl: "#ffad42",
+//   pd: "#bb4d00",
+
+//   bg: "#212121",
+//   bgl: "#484848",
+//   bgd: "#000000",
+// };
 
 export default App;
